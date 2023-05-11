9d018428a2625e55a1eb0e4ce5f42de73903ff6a
TomZheng <1219.zheng@gmail.com>
2023-05-10 23:49:19 -0700

    add new country

--- fleetapp/src/main/java/com/vehiclemanagement/fleetapp/controller/CountryController.java
+++ fleetapp/src/main/java/com/vehiclemanagement/fleetapp/controller/CountryController.java
@@ -1,23 +1,35 @@
 package com.vehiclemanagement.fleetapp.controller;
 
+import com.vehiclemanagement.fleetapp.model.Country;
 import com.vehiclemanagement.fleetapp.service.CountryService;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @Controller
 public class CountryController {
-	
-	@Autowired private CountryService countryService;
+
+	@Autowired
+	private CountryService countryService;
 
 	//Get All Countrys
-	@GetMapping("countries")
+	@GetMapping("/countries")
 	public String findAll(Model model){
-		model.addAttribute("countries", countryService.findAll());
+		List<Country> countryList = countryService.getCountries();
+		model.addAttribute("countries", countryList);
 		return "Country";
+	}
+
+	@PostMapping("/countries/addNew")
+	public String addNew(Country country) {
+		countryService.save(country);
+		return "redirect:/countries";
 	}
 
 //	@RequestMapping("countries/findById")
--- fleetapp/src/main/java/com/vehiclemanagement/fleetapp/service/CountryService.java
+++ fleetapp/src/main/java/com/vehiclemanagement/fleetapp/service/CountryService.java
@@ -12,28 +12,39 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CountryService {
-	
+
 	@Autowired
 	private CountryRepository countryRepository;
-	
+
 	//Get All Countrys
-	public List<Country> findAll(){
+	public List<Country> getCountries(){
 		return countryRepository.findAll();
-	}	
-	
-	//Get Country By Id
-	public Optional<Country> findById(int id) {
-		return countryRepository.findById(id);
-	}	
-	
-	//Delete Country
-	public void delete(int id) {
-		countryRepository.deleteById(id);
 	}
-	
-	//Update Country
-	public void save( Country country) {
+
+	//Save new country
+	public void save(Country country) {
 		countryRepository.save(country);
 	}
+
+
+//	//Get All Countrys
+//	public List<Country> findAll(){
+//		return countryRepository.findAll();
+//	}
+//
+//	//Get Country By Id
+//	public Optional<Country> findById(int id) {
+//		return countryRepository.findById(id);
+//	}
+//
+//	//Delete Country
+//	public void delete(int id) {
+//		countryRepository.deleteById(id);
+//	}
+//
+//	//Update Country
+//	public void save( Country country) {
+//		countryRepository.save(country);
+//	}
 
 }
