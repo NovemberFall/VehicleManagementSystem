cb9af42437ff618bec20a73e03c5b0d429dc784c
TomZheng <1219.zheng@gmail.com>
2023-05-10 20:56:07 -0700

    update index

---
+++ fleetapp/src/main/java/com/vehiclemanagement/fleetapp/service/CountryService.java
@@ -0,0 +1,39 @@
+package com.vehiclemanagement.fleetapp.service;
+
+import com.vehiclemanagement.fleetapp.model.Country;
+import com.vehiclemanagement.fleetapp.repository.CountryRepository;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class CountryService {
+	
+	@Autowired
+	private CountryRepository countryRepository;
+	
+	//Get All Countrys
+	public List<Country> findAll(){
+		return countryRepository.findAll();
+	}	
+	
+	//Get Country By Id
+	public Optional<Country> findById(int id) {
+		return countryRepository.findById(id);
+	}	
+	
+	//Delete Country
+	public void delete(int id) {
+		countryRepository.deleteById(id);
+	}
+	
+	//Update Country
+	public void save( Country country) {
+		countryRepository.save(country);
+	}
+
+}
